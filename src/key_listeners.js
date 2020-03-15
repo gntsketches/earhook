b020f8225253e844f87168ebb31f7070cf6de12d
Gregory Tippett <gntsketches@gmail.com>
2020-03-15 07:03:48 -0700

    add keydown-repeat block'

--- src/key_listeners.js
+++ src/key_listeners.js
@@ -1,13 +1,26 @@
 import { keyToNote } from "./constants"
 
+let down = []
+// use of file scope, but cleaner than another call to setState
+
 export function addListeners(scope) {
   document.addEventListener('keydown', function(e) {
-  //   console.log(e.key)
-    if (e.key === ' ') {
-      scope.startStop()
+    // console.log('down', down)
+    if (down.indexOf(e.key) === -1) {
+      down.push(e.key)
+      if (e.key === ' ') {
+        scope.startStop()
+      }
+      else if (e.key in keyToNote) {
+        scope.sendResponse(keyToNote[e.key])
+      }
     }
-    else if (e.key in keyToNote) {
-      scope.sendResponse(keyToNote[e.key])
+  })
+
+  document.addEventListener('keyup', function(e) {
+      // console.log('down', down)
+    if (down.indexOf(e.key) > -1) {
+      down = down.filter(k => k !== e.key)
     }
   })
 }
